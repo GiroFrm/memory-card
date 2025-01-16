39875d80844c253894475241d791be58f8aad79d
GiroFrm <girofram844@gmail.com>
2025-01-16 18:01:04 +1100

    feat: Add score tracking and card shuffling logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import {shuffled} from './util';
+import Card from './Card';
+
+const pokemonNames = [
+  "charizard",
+  "pikachu",
+  "bulbasaur",
+  "lucario",
+  "gengar",
+  "greninja",
+  "eevee",
+  "gardevoir",
+  "umbreon",
+  "dragonite"
+]; 
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const[score, setScore] = useState(0);
+  const [highestScore, setHighestScore] = useState(0);
+  const[clicked, setClicked] = useState([]);
+
+  useEffect(() => {
+    Promise.all(pokemonNames.map(name => 
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(response => response.json()))
+    ).then((jsons) => {
+        const newPokemons = jsons.map((json) => {
+            return {name: json["name"], art:json["sprites"]["front_default"]}
+        });
+    
+        setPokemons(newPokemons);
+    });
+},[]);
+
+
+function handleClick(pokemonName){
+ 
+  setClicked((prevClicked) => {
+    const isClicked = prevClicked.includes(pokemonName);
+
+    if (isClicked) {
+      setHighestScore((prevHighestScore) => Math.max(prevHighestScore, score));
+      setScore(0);
+      return [];
+    } else {
+      setScore((prevScore) => prevScore + 1);
+      setPokemons((prevPokemons) => shuffled([...prevPokemons]));
+      return [...prevClicked, pokemonName];
+      
+    }
+  });
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ 
+    <div className="container">
+      <header className="header">
+      <p>Get points by clicking on an image but don't click on any more than once! {clicked}</p>
+      <p>Score: {score}</p>
+      <p>HighestScore: {highestScore}</p>
+      </header>
+      <div className='cards-wrapper'>
+      { pokemons.map((pokemon)=>(
+       <Card
+       key={pokemon.name}
+       cardInfo={pokemon}
+       clickHandler={() => handleClick(pokemon.name)}
+   />
+      ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  
   )
 }
 
